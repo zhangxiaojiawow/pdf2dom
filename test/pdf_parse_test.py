43ee2ab1be7f1012b10a22088cfd817dbfe1a244
zhangxiaojia002 <zhangxiaojia002@ke.com>
2023-12-05 17:39:30 +0800

    测试脚本

--- test/pdf_parse_test.py
+++ test/pdf_parse_test.py
@@ -1,15 +1,16 @@
 from pdf2docx import Converter, parse
 
 test_dir = '../test_document/'
-converter = Converter(test_dir+"外泌体.pdf")
-tables = converter.extract_tables(start=0, end=2,
+converter = Converter(test_dir+"办公.pdf")
+# converter.convert(test_dir+"办公.docx", start=0, end=1)
+tables = converter.extract_tables(start=1, end=2,
                                   extract_table_with_cell_pos=True,
                                   remove_watermark=True,
                                   debug=True,
-                                  debug_file_name=test_dir+"页眉页脚测试-debug.pdf",
+                                  debug_file_name=test_dir+"办公-debug.pdf",
                                   sematic_parse=True,
                                   parse_stream_table=False)
-print(tables)
+# print(tables)
 
 # converter.convert(start=0, end=1, docx_filename=test_dir+"大连.docx")
 
